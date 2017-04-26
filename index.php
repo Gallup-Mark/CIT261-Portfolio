855b873e13bbefdaba33efc4c885ea0c70c6fd8b
Mark Gallup <ke6tzc@gmail.com>
2017-04-25 19:06:20 -0700

    4/25/17 First commit of CIT 261 Portfolio

---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
+        <title>CIT261 - Spring 2017 Portfolio</title>
+        <style type="text/css"></style>
+    </head>
+
+    <body>
+        <h1 style="text-align: left;"><img style="width: 117px; height: 90px;" src="BYUi Logo.png" alt=""></h1>
+        <h1 style="text-align: left;">Spring 2017 - CIT-261&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;
+            &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
+            &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
+            &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;</h1>
+        <h2>Portfolio:&nbsp; Mark Gallup</h2>
+        <table style="text-align: left; width: 100%;" border="1" cellpadding="2" cellspacing="2">
+            <tbody>
+                <tr>
+                    <td style="vertical-align: top;"><big><big><span style="font-weight: bold;">Code Topics</span></big></big><br>
+            </td>
+            <td style="vertical-align: top;"><big><big><span style="font-weight: bold;">Last Updated</span><br>
+                </big></big></td>
+        <td style="vertical-align: top;"><big><big><span style="font-weight: bold;">Notes to Bro. James</span><br>
+            </big></big></td>
+</tr>
+<tr>
+    <td style="vertical-align: top;">JavaScript<br>
+    </td>
+    <td style="vertical-align: top;">Not Ready<br>
+    </td>
+    <td style="vertical-align: top;"><br>
+    </td>
+</tr>
+<tr>
+    <td style="vertical-align: top;">JSON<br>
+    </td>
+    <td style="vertical-align: top;">Not Ready<br>
+    </td>
+    <td style="vertical-align: top;"><br>
+    </td>
+</tr>
+<tr>
+    <td style="vertical-align: top;">Local Storage<br>
+    </td>
+    <td style="vertical-align: top;">Not Ready<br>
+    </td>
+    <td style="vertical-align: top;"><br>
+    </td>
+</tr>
+<tr>
+    <td style="vertical-align: top;">DOM<br>
+    </td>
+    <td style="vertical-align: top;">Not Ready<br>
+    </td>
+    <td style="vertical-align: top;"><br>
+    </td>
+</tr>
+<tr>
+    <td style="vertical-align: top;">CSS3 Transitions<br>
+    </td>
+    <td style="vertical-align: top;">Not Ready<br>
+    </td>
+    <td style="vertical-align: top;"><br>
+    </td>
+</tr>
+<tr>
+    <td style="vertical-align: top;">CSS3 Transformations<br>
+    </td>
+    <td style="vertical-align: top;">Not Ready<br>
+    </td>
+    <td style="vertical-align: top;"><br>
+    </td>
+</tr>
+<tr>
+    <td style="vertical-align: top;">CSS3 Animations<br>
+    </td>
+    <td style="vertical-align: top;">Not Ready<br>
+    </td>
+    <td style="vertical-align: top;"><br>
+    </td>
+</tr>
+<tr>
+    <td style="vertical-align: top;">CSS3+JavaScript<br>
+    </td>
+    <td style="vertical-align: top;">Not Ready<br>
+    </td>
+    <td style="vertical-align: top;"><br>
+    </td>
+</tr>
+<tr>
+    <td style="vertical-align: top;">AJAX<br>
+    </td>
+    <td style="vertical-align: top;">Not Ready<br>
+    </td>
+    <td style="vertical-align: top;"><br>
+    </td>
+</tr>
+<tr>
+    <td style="vertical-align: top;">JavaScript Events<br>
+    </td>
+    <td style="vertical-align: top;">Not Ready<br>
+    </td>
+    <td style="vertical-align: top;"><br>
+    </td>
+</tr>
+<tr>
+    <td style="vertical-align: top;"><br>
+    </td>
+    <td style="vertical-align: top;"><br>
+    </td>
+    <td style="vertical-align: top;"><br>
+    </td>
+</tr>
+</tbody>
+</table>
+<br>
+<br>
+<footer> </footer>
+<ul>
+    <li><a href="mailto:byufan@me.com">Email: Byufan@me.com</a></li>
+</ul>
+
+
+</body>
+
+</html>
+
